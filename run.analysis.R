5f0d116a2baac7f48bdf048adbf0aed5d5e2d847
Yousri Hajri <yhajri1976@gmail.com>
2017-11-27 18:20:13 +0100

    Update run.analysis.R

--- run.analysis.R
+++ run.analysis.R
@@ -8,7 +8,8 @@ library(stringr)
 ##Extract features and activity labels
 ##Extract subject_test X_test & y_test 
 ##Extract subject_train X_train & y_train 
-source("C:\\Users\\yousri.hajri\\Desktop\\DATA SCIENCE\\3 GETTING AND CLEANING DATA\\WEEK4\\run_analysis.R\\data.extractor.R")
+setwd(dir = "C:\\Users\\yousri.hajri\\Documents\\DATA_SCIENCE\\3_GETTING_AND_CLEANING_DATA\\WEEK4\\run_analysis.R")
+source("data.extractor.R")
 
 ##Merge train&test
 
@@ -18,24 +19,20 @@ y <- rbind.data.frame(y_train, y_test)
 
 ##Merge y and activity_labels
 
-for (i1 in 1:length(y$V1)) {
-  for (i2 in 1:6)          {
-                            if (y$V1[[i1]]==activity_labels$V1[[i2]]) {
-                              y$V1[[i1]] <- as.character (activity_labels$V2[[i2]])} 
-}
-}
+y <- merge (x=y, y=activity_labels, by.x = "V1", by.y = "V1", sort=F) 
+y <- select (y, V2) %>% rename(activity_labels=V2)
 
 ##Merge X and features
+
 X <- as.data.frame(X)
 X <- rbind(as.vector(features$V2), X)
 colnames(X)=X[1,]
 X <- X[-1,]
 X <- as.data.table(X)
 
-##Rename columns of subject & y
+##Rename column of subject
 
 subject <- rename (subject, subject=V1)
-y <- rename (y, activity_labels=V1)
 
 ##Fulldata merger
 
@@ -56,18 +53,18 @@ v1 <- c()
 for (i in 3:length(colnames(fulldata))) {
               if (length(grep("mean", colnames(fulldata[,i,with=FALSE])))==1) {
                v1 <- c(v1,i)      }}
-meanfulldata <- fulldata[,c(1,2,v1),with=FALSE]
+mean_fulldata <- fulldata[,c(1,2,v1),with=FALSE]
 
 ##Create stdfulldata with colnames containing "std"
 v2 <- c()
 for (i in 3:length(colnames(fulldata))) {
   if (length(grep("std", colnames(fulldata[,i,with=FALSE])))==1) {
     v2 <- c(v2,i)      }}
-stdfulldata <- fulldata[,v2,with=FALSE]
+std_fulldata <- fulldata[,v2,with=FALSE]
 
 ##Merge meanfulldata and stdfulldata in subdata
 
-subdata <- cbind (meanfulldata, stdfulldata)
+subdata <- cbind (mean_fulldata, std_fulldata)
 
 ##Class the columns of subdata and make 2-digit subject
 
@@ -75,46 +72,24 @@ collength <- length(names(subdata))
 
 for(i in 3:collength) {
               subdata[,i] <- as.numeric (as.matrix(subdata[,i,with=FALSE])) }
-subdata <- arrange (subdata, subject)
-subdata$subject <- as.character (subdata$subject)
-for (i in 1:nrow(subdata)) { if (as.numeric(subdata [i, "subject"]) <=9)
-                    {subdata [i, "subject"] <- paste ("0",subdata [i, "subject"],
-                                              sep = "")
-}}
 
 subdata$activity_labels <- as.factor(subdata$activity_labels)
 subdata$subject <- as.factor(subdata$subject)
 
 subdata <- tbl_df(subdata)
 
+subdata <- arrange (subdata, subject)
 
-##Create meandata with mean of each subject for each activity
+##Summarise all subject/activity_labels by mean function
 
-subdata2 <- subdata %>%
-  arrange(activity_labels) %>%
-  arrange(subject) %>%
-  mutate (newcol = paste(subject, activity_labels))
-
-subdata3 <- cbind (subdata2$newcol, subdata2)
-
-subdata3 <- subdata3 %>%
-  select (-2,-3,-newcol) %>%
-  rename(V='subdata2$newcol') %>%
-  group_by(V) 
-
-meandata <- summarise_all (subdata3, funs(mean))
-meandata <- arrange (meandata, V)
-excolumns <- as.data.frame (str_split_fixed(meandata$V, " ", 2))
-excolumns <- rename(excolumns, subject = V1, activity_labels=V2)
-meandata <- cbind (excolumns, meandata)
-meandata <- meandata [,-3 ]
-meandata <- tbl_df(meandata)
-
+subdata <- subdata %>%
+  group_by(subject, activity_labels) %>%
+  summarise_all (funs(mean))
 
 
 ##Remove old data
 
-remove (fulldata, meanfulldata, stdfulldata, subdata2, subdata3, excolumns)
+remove (fulldata, mean_fulldata, std_fulldata)
 
 
 ##Performance
